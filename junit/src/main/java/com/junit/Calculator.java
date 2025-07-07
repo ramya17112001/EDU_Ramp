795b43d6d936c7194644c596c0fd53c1bc19a4ff
ramya17112001 <gollapalliramya123@gmail.com>
2025-07-07 12:34:10 +0530

    first project

---
+++ junit/src/main/java/com/junit/Calculator.java
@@ -0,0 +1,21 @@
+package com.junit;
+
+public class Calculator{
+	public int add(int a, int b) {
+
+		int c=a+b;
+		return c;   
+	}
+	public int sub(int a ,int b) {
+			int c=a-b;
+			return c;   
+			}
+	public int mul(int a ,int b) {
+		int c=a*b; 
+		return c;   
+		} 
+	public int div(int a ,int b) {
+		int c=a/b; 
+		return c;     
+		} 
+} 
