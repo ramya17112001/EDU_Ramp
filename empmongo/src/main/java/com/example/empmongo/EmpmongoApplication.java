3d50e7ebfc9c57ddf0883690130ba6fb6701a17f
ramya17112001 <gollapalliramya123@gmail.com>
2025-07-15 18:19:39 +0530

    employeeproject mongo added

---
+++ empmongo/src/main/java/com/example/empmongo/EmpmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.empmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmongoApplication.class, args);
+	}
+
+}
---
+++ empmongo/src/test/java/com/example/empmongo/EmpmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
